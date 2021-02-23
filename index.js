d9675e8b8a0b5360daac5a7f708a6a976983fbc6
parisa-06 <75797030+parisa-06@users.noreply.github.com>
2021-02-23 09:24:15 +0330

    Bug fixes number_waterWell

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
- var  i, j, k=1;
- var lenght_home   = prompt("Enter your number home : ");
- var number_waterWell   = prompt("Enter your number_waterWell   : ");
+ var i, j, k = 1;
+ var lenght_home = prompt("Enter your number home : ");
+ var number_waterWell = prompt("Enter your number_waterWell   : ");
  var number_jump = prompt("Enter your number_jump: ");
  var home = new Array();
- var jump  = new Array();
- var Well=new Array()
+ var jump = new Array();
+ var Well = new Array()
  var p = 0;
- var w =0;
+ var w = 0;
 
- for(i=1; i<= number_waterWell; i++){
-  
-    random_holds = Math.floor(Math.random() * (number_waterWell)+1);
-    if(home[random_holds] == null)
-      home[random_holds]= "waterWell"
-    else
-      (i--)
-      document.getElementById("demo1").innerHTML= home
-}
+ for (i = 1; i <= number_waterWell; i++) {
 
- for(j=0; j<lenght_home && k<=number_jump ; j+=4){
-   
-     if( home[j] == "waterWell" ){
-        Well[w] =j;
-        document.getElementById("demo2").innerHTML = Well;
-        w++; }
-       
-     else{
-        jump[p]=j;
-         document.getElementById("demo3").innerHTML=jump;
-        p++;
-        k++
-    }
-    
-  
-}
+     random_holds = Math.floor(Math.random() * (number_waterWell) + 1);
+     if (home[random_holds] == null) {
+         home[random_holds] = "waterWell";
+         Well[w] = i;
+         document.getElementById("demo2").innerHTML = Well;
+         w++;
+     } else
+         (i--)
+     document.getElementById("demo1").innerHTML = home;
+ }
 
+ for (j = 0; j < lenght_home && k <= number_jump; j += 4) {
+
+     if (home[j] != "waterWell") {
+         jump[p] = j;
+         document.getElementById("demo3").innerHTML = jump;
+         p++;
+         k++
+     }
+
+
+ }
